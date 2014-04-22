b549f13d4b89f79600e788f3267490c84c6e8c0b
Maksym Borodin <borodin.maksym@gmail.com>
2014-04-22 17:57:02 +0200

    Added translations

---
+++ translations/i18n_pl.ts
@@ -0,0 +1,216 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl_PL">
+<context>
+    <name>CopyableElideLabel</name>
+    <message>
+        <location filename="../src/copyableelidelabel.cpp" line="29"/>
+        <source>Copy</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CreateNewAccountWizardPage</name>
+    <message>
+        <location filename="../src/createnewaccountwizardpage.cpp" line="12"/>
+        <source>Username:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/createnewaccountwizardpage.cpp" line="17"/>
+        <source>Password:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>EmojiFontSettingsDialog</name>
+    <message>
+        <location filename="../src/Settings/emojifontsettingsdialog.cpp" line="33"/>
+        <source>Emoji Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/Settings/emojifontsettingsdialog.cpp" line="35"/>
+        <source>Override native font for Emoji</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/Settings/emojifontsettingsdialog.cpp" line="46"/>
+        <source>Reset</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/Settings/emojifontsettingsdialog.cpp" line="61"/>
+        <source>Emoticons preview</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>GuiSettingsPage</name>
+    <message>
+        <location filename="../src/Settings/guisettingspage.cpp" line="106"/>
+        <source>Smooth animation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/Settings/guisettingspage.cpp" line="108"/>
+        <source>Enable animation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/Settings/guisettingspage.cpp" line="116"/>
+        <source>Smileypack</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/Settings/guisettingspage.cpp" line="183"/>
+        <source>&lt;b&gt;%1&lt;/b&gt;%2 by %3&lt;br&gt;&quot;&lt;i&gt;%4&lt;/i&gt;&quot;%5</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/Settings/guisettingspage.cpp" line="197"/>
+        <source>Others</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/Settings/guisettingspage.cpp" line="199"/>
+        <source>Minimize to tray on close</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>InputTextWidget</name>
+    <message>
+        <location filename="../src/inputtextwidget.cpp" line="36"/>
+        <source>Type your text here...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/inputtextwidget.cpp" line="41"/>
+        <source>Undo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/inputtextwidget.cpp" line="42"/>
+        <source>Redo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/inputtextwidget.cpp" line="43"/>
+        <source>Cut</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/inputtextwidget.cpp" line="44"/>
+        <source>Copy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/inputtextwidget.cpp" line="45"/>
+        <source>Paste</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LoginWindow</name>
+    <message>
+        <location filename="../src/loginwindow.cpp" line="60"/>
+        <source>User name:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/loginwindow.cpp" line="77"/>
+        <source>Password:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/loginwindow.cpp" line="89"/>
+        <source>Login</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/loginwindow.cpp" line="96"/>
+        <source>Create account</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/loginwindow.cpp" line="100"/>
+        <source>Import</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="74"/>
+        <source>Add friend</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="82"/>
+        <source>Menu</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="85"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="87"/>
+        <source>About %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="88"/>
+        <source>About Qt</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="90"/>
+        <location filename="../src/mainwindow.cpp" line="183"/>
+        <source>Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="170"/>
+        <location filename="../src/mainwindow.cpp" line="293"/>
+        <source>Hide</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="289"/>
+        <source>Show</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MessageDisplayWidget</name>
+    <message>
+        <location filename="../src/messagedisplaywidget.cpp" line="152"/>
+        <source>Couldn&apos;t send the message!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MessageLabel</name>
+    <message>
+        <location filename="../src/messagelabel.cpp" line="35"/>
+        <source>Copy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/messagelabel.cpp" line="42"/>
+        <source>Copy Message</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../src/Settings/settingsdialog.cpp" line="30"/>
+        <source>GUI</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
